30c0fdf4fb7ebee1fe3b6c8b7c8819cea3a9286e
Tom Ridge <tom.j.ridge@googlemail.com>
2016-08-04 21:55:02 +0100

    Almost lost this... the new makefile

--- fs_spec/config.sh
+++ fs_spec/config.sh
@@ -1,1 +1,10 @@
-build/config.sh
+# export all vars
+set -a 
+
+test -f ../../config.sh && . ../../config.sh
+
+CPPO_ARGS="-D aspect_perms"
+LEMFLAGS="-lib $LEMLIB -only_changed_output -wl_unused_vars ign -wl_rename err"
+PKGS="-package sexplib,sexplib.syntax,sha -syntax camlp4o"
+ocamlc="ocamlfind ocamlc -I $EXTRACTDIR extract.cma $PKGS"
+ocamlopt="ocamlfind ocamlopt -I $EXTRACTDIR extract.cmxa $PKGS"
